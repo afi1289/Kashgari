5106ae59f32e50e3385083334613f2870af88945
AFI <46696930+afi1289@users.noreply.github.com>
2021-02-05 04:16:19 +0100

    Update __init__.py

--- kashgari/tasks/labeling/__init__.py
+++ kashgari/tasks/labeling/__init__.py
@@ -13,6 +13,7 @@ from .bi_gru_crf_model import BiGRU_CRF_Model
 from .bi_lstm_model import BiLSTM_Model
 from .bi_lstm_crf_model import BiLSTM_CRF_Model
 from .cnn_lstm_model import CNN_LSTM_Model
+from .double_blstm import DoubleBLSTMModel
 
 ALL_MODELS = [
     BiGRU_Model,
@@ -20,6 +21,7 @@ ALL_MODELS = [
     BiLSTM_Model,
     BiLSTM_CRF_Model,
     CNN_LSTM_Model,
+    DoubleBLSTMModel,
 ]
 
 if __name__ == "__main__":
